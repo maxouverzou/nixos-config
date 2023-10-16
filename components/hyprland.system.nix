debbc3a4a451401939ef6e226c8d5fd748b1e4e6
Maxou Verzou <maxou@wheatley>
2023-10-15 20:14:07 -0700

    hyprland: fix for thinkpad-e11

--- components/hyprland.system.nix
+++ components/hyprland.system.nix
@@ -1,10 +1,12 @@
-{pkgs, ...}: {
+{pkgs, config, ...}: {
   # https://github.com/NixOS/nixpkgs/issues/143365
   security.pam.services.swaylock = { };
 
   services.geoclue2.enable = true;
 
-  services.blueman.enable = hardware.bluetooth.enable;
+  services.blueman.enable = config.hardware.bluetooth.enable;
 
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+
+  hardware.opengl.enable = true;
 }
