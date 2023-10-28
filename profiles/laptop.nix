7c2710205ea57c6299d371a1f4a189a1eca447c5
Maxou Verzou <maxou@wheatley>
2023-10-27 23:03:08 -0700

    laptop: enable hibernation

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -1,9 +1,37 @@
 {
-  # these two options are conflicting...
-  #services.tlp.enable = true;
-  #powerManagement.powertop.enable = true;
-  #services.upower.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-  # if services.xserver.desktopManager.plasma5.enable
-  # -> pkgs powerdevil 
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+    };
+  };
+  powerManagement.enable = true;
+
+  services.logind = {
+    lidSwitch = "suspend-then-hibernate";
+    # TODO: support more options
+    # https://www.man7.org/linux/man-pages/man5/logind.conf.5.html
+    extraConfig = ''
+      HandlePowerKey=suspend-then-hibernate
+      IdleAction=suspend-then-hibernate
+      IdleActionSec=2m
+    '';
+  };
+
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
+
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=1h
+    SuspendState=mem
+  '';
+
 }
