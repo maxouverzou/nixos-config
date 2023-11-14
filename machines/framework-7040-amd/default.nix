e2947bdfb1caa35fbfb7d28dcf18285318ded0c4
Maxou Verzou <maxou@integratedintegrals.com>
2023-11-14 14:48:15 -0800

    framework-7040-amd: import new nixos-hardware profile

--- machines/framework-7040-amd/default.nix
+++ machines/framework-7040-amd/default.nix
@@ -9,32 +9,13 @@
 in {
   imports =
     [
-      nixos-hardware.common-cpu-amd
-      nixos-hardware.common-cpu-amd-pstate
-      nixos-hardware.common-gpu-amd
-      nixos-hardware.common-pc-laptop
-      nixos-hardware.common-pc-laptop-ssd
+      nixos-hardware.framework-13-7040-amd
       ./hardware-configuration.nix
     ];
 
   services.tlp.enable = lib.mkForce false;
   services.fwupd.enable = true;
-
-  /* TODO https://github.com/NixOS/nixos-hardware/pull/765 */
-  services.power-profiles-daemon.enable = true;
-
-  boot.extraModprobeConfig = ''
-    options snd-hda-intel model=dell-headset-multi
-  '';
-
   services.fprintd.enable = true;
-
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="8156", ATTR{power/autosuspend}="20"
-  '';
-
-  hardware.sensor.iio.enable = true;
-
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
