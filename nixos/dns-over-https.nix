e50277704a3a43765e485a355dcab7190302f8cc
Maxou Verzou <maxou@integratedintegrals.com>
2024-01-26 10:30:51 -0800

    hardcode archive.is ip

--- nixos/dns-over-https.nix
+++ nixos/dns-over-https.nix
@@ -9,4 +9,12 @@
   systemd.services.dnscrypt-proxy2.serviceConfig = {
     StateDirectory = "dnscrypt-proxy";
   };
+
+  # archive.is blocks cloudflare dns: https://news.ycombinator.com/item?id=19828702
+  networking.extraHosts =
+    ''
+      41.77.143.21 archive.is
+      41.77.143.21 archive.ph
+      41.77.143.21 archive.today
+    '';  
 }
