fccdde6c89d34cf54aaf3c696396ea02aa3cea76
Maxou Verzou <maxou@integratedintegrals.com>
2024-04-20 18:05:18 -0700

    glados: try to fix virtualisation

--- hosts/glados/default.nix
+++ hosts/glados/default.nix
@@ -16,7 +16,24 @@
 
   virtualisation.libvirtd = {
     enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+    };
   };
+
+  # not needed, firewall is already disabled below
+  # networking.firewall.trustedInterfaces = [ "virbr0" ];
 
   /* remove these once the migration to btrfs is done */
   boot.supportedFilesystems = [ "btrfs" "ext4" ];
