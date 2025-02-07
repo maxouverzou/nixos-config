27b2c869c737e72c735e35a9fe2f9808bc4c9ee3
Maxou Verzou <maxou@integratedintegrals.com>
2025-02-06 16:21:06 -0800

    configure paperless admin password

--- hosts/gertry-3000/services/paperless.nix
+++ hosts/gertry-3000/services/paperless.nix
@@ -1,6 +1,14 @@
-{
+{ config, ...}: {
+  sops.secrets = {
+    PAPERLESS_ADMIN_PASSWORD = {
+      mode = "0400";
+      owner = config.services.paperless.user;
+    };
+  };
+
   services.paperless = {
     enable = true;
+    address = "0.0.0.0";
+    passwordFile = config.sops.secrets.PAPERLESS_ADMIN_PASSWORD.path;
   };
 }
-
